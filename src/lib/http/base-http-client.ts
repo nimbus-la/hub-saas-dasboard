86defe5a934ce92f0dfe2e2a9711951b4315c529
Stebanm <juanesxde16@gmail.com>
2026-08-14 11:35:17 -0500

    Refactor (Api): Se realiza mejora y refactorizacion del cliente de peticiones http

---
+++ src/lib/http/base-http-client.ts
@@ -0,0 +1,79 @@
+import type { HttpClient, HttpRequest, HttpRequestConfig, HttpResponse } from "@/interfaces";
+
+
+export abstract class BaseHttpClient implements HttpClient {
+    public abstract request<TData>(request: HttpRequest): Promise<HttpResponse<TData>>;
+
+
+    public async get<TData>(url: string, config?: HttpRequestConfig): Promise<TData> {
+        const { data } = await this.request<TData>({
+            ...config,
+            method: "GET",
+            url
+        });
+
+        return data;
+    }
+
+
+    public async post<TData>(
+        url: string,
+        body?: unknown,
+        config?: HttpRequestConfig
+    ): Promise<TData> {
+        const { data } = await this.request<TData>({
+            ...config,
+            method: "POST",
+            url,
+            body
+        });
+
+        return data;
+    }
+
+
+    public async put<TData>(
+        url: string,
+        body?: unknown,
+        config?: HttpRequestConfig
+    ): Promise<TData> {
+        const { data } = await this.request<TData>({
+            ...config,
+            method: "PUT",
+            url,
+            body
+        });
+
+        return data;
+    }
+
+
+    public async patch<TData>(
+        url: string,
+        body?: unknown,
+        config?: HttpRequestConfig
+    ): Promise<TData> {
+        const { data } = await this.request<TData>({
+            ...config,
+            method: "PATCH",
+            url,
+            body
+        });
+
+        return data;
+    }
+
+
+    public async delete<TData>(
+        url: string,
+        config?: HttpRequestConfig
+    ): Promise<TData> {
+        const { data } = await this.request<TData>({
+            ...config,
+            method: "PUT",
+            url,
+        });
+
+        return data;
+    }
+}
