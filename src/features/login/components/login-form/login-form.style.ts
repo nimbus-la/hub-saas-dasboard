5f73bb5adb82a63a2ae91eb7145007ad0019b38f
Luna1808 <lunsbc18@gmail.com>
2026-08-16 20:14:59 -0500

    feat: implementar y ajustar responsive del login

--- src/features/login/components/login-form/login-form.style.ts
+++ src/features/login/components/login-form/login-form.style.ts
@@ -1,29 +1,29 @@
 import { cva } from "class-variance-authority";
 
 export const loginFormVariants = cva(
-    "w-full"
+  "w-full max-w-md"
 );
 
 export const loginFormHeaderVariants = cva(
-    "flex flex-col gap-2"
+  "flex flex-col gap-2"
 );
 
 export const loginFormFieldsVariants = cva(
-    "flex flex-col gap-5"
+  "flex flex-col gap-5"
 );
 
 export const loginFormOptionsVariants = cva(
-    "flex items-center justify-between gap-4"
+  "flex items-center justify-between gap-4"
 );
 
 export const loginFormSocialVariants = cva(
-    "grid grid-cols-2 gap-3"
+  "grid grid-cols-2 gap-3"
 );
 
 export const loginFormDividerVariants = cva(
-    "flex items-center gap-4"
+  "flex items-center gap-4"
 );
 
 export const loginFormFooterVariants = cva(
-    "text-center"
+  "text-center"
 );
